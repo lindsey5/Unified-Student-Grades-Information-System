cd2cbe3de98e755137db2305d8e71bbd3034c4a1
lindsey5 <lindseysamson5@gmail.com>
2025-11-26 03:30:41 +0800

    commit

--- frontend/src/hooks/useProtection.tsx
+++ frontend/src/hooks/useProtection.tsx
@@ -1,19 +1,12 @@
-import { useEffect } from "react";
-import { useNavigate } from "react-router-dom";
 import useFetch from "./useFetch";
 
 const useProtection = () => {
     const { data, loading } = useFetch('/api/auth/user');
-    const navigate = useNavigate();
 
-    useEffect(() => {
-        if (loading) return;
-
-        if (data?.role === "student") navigate("/student");
-        else if (data?.role === "registrar") navigate("/registrar");
-        else if (data?.role === "admin") navigate("/admin");
-
-    }, [loading, data]);
+    if(loading) return
+    else if (data?.role === "student")  window.location.href = "/student"
+    else if (data?.role === "registrar")  window.location.href = "/registrar"
+    else if (data?.role === "admin")  window.location.href = "/admin"
 };
 
 export default useProtection;
